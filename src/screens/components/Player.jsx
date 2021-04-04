b3afb2adb132258ccaf5f539b34cb77be71f5fd3
ChabaJosa <chabagjg@gmail.com>
2021-04-04 10:02:59 -0400

    Updated resume

--- src/screens/components/Player.jsx
+++ src/screens/components/Player.jsx
@@ -1,23 +1,20 @@
 import React from "react";
-import {
-  MDBContainer,
-  MDBIframe 
-} from "mdbreact";
+import { MDBContainer } from "mdbreact";
 
 export default function ProjectModal(props) {
   return (
     <React.Fragment>
       <MDBContainer>
-            <div className="embed-responsive embed-responsive-16by9">
-              <iframe
-                title={props.title}
-                className="embed-responsive-item"
-                id="ytplayer"
-                type="text"
-                src={`https://www.youtube.com/embed/${props.videoID}`}
-                frameBorder="0"
-              ></iframe>
-            </div>
+        <div className="embed-responsive embed-responsive-16by9">
+          <iframe
+            title={props.title}
+            className="embed-responsive-item"
+            id="ytplayer"
+            type="text"
+            src={`https://www.youtube.com/embed/${props.videoID}`}
+            frameBorder="0"
+          ></iframe>
+        </div>
       </MDBContainer>
     </React.Fragment>
   );
